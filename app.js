51e6c41ea5ac538b58f85f74bb06573ff6a5963a
AndreeaCalaver <andreeacalaver97@gmail.com>
2023-05-15 18:00:28 +0100

    Implement square root functionality

--- app.js
+++ app.js
@@ -6,20 +6,24 @@ let accumulativeCalculation
 
 
 function calculate(button){
-    
-
     const value = button.textContent;
     if(value === "CLEAR"){
         calculation = []
         screenDisplay.textContent = "."
     } else if(value === "="){
         screenDisplay.textContent = eval(accumulativeCalculation)
-    } else {
-        calculation.push(value);
-    accumulativeCalculation = calculation.join("")
-    screenDisplay.textContent = accumulativeCalculation;
+    } else if( value === "√"){
+        screenDisplay.textContent = Math.sqrt(eval(accumulativeCalculation) );
+        accumulativeCalculation = screenDisplay.textContent;
+        calculation = [];
+        calculation.push(screenDisplay.textContent);
     }
-    
-    
+    else {
+        calculation.push(value);
+        
+        accumulativeCalculation = calculation.join("");
+
+        screenDisplay.textContent = accumulativeCalculation;
+    }
 }
 buttons.forEach(button => button.addEventListener("click", () => calculate(button) ));
